707094e59322e1fa3baa9427dd4f66a202753b83
kevin <kevin.sy@devinci.fr>
2017-01-27 13:21:10 +0100

    étape 1 et 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// on importe les composants présents sur l'app
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { GamesService } from './games.service';
+
+// on importe les services
+import {TodoService } from "./todo.service";
 
 @NgModule({
+  // on déclare les composants pour pouvoir les utiliser
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  // on déclare son service pour que chaque composant l'utilise
+  providers: [TodoService,GamesService],
+
+  // point de départ de l'app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
